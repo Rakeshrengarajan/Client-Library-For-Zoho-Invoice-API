a84c772f56db15df718d39d49300e2ff91235d87
Rakesh <rakeshrengarajan@gmail.com>
2021-07-09 11:48:37 +0530

    Add files via upload

---
+++ Invoice Client Library/lib/response.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+
+# Module Dependencies
+import os
+import util
+from dotenv import load_dotenv
+import requests
+
+class Response:
+    def __init__(self):
+        self.organizationId=os.getenv('organizationId')
+        code=util.Utils()
+        self.authcode=code.refreshaccessToken()
+        self.custom_header = {
+            "Authorization": "Zoho-oauthtoken "+self.authcode,
+            "X-in-zoho-invoice-organizationid":self.organizationId,
+            "Content-Type":"application/json"
+        }
+
+    def getResposnse(self,method,url,parameters=None):
+        res=requests.request(method,url,headers=self.custom_header,params=parameters)
+        return res.json()
