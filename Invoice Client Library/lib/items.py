f79213e9d5b25045478626b641de826c77cf9e56
Rakesh <rakeshrengarajan@gmail.com>
2021-07-09 11:51:36 +0530

    Update items.py

--- Invoice Client Library/lib/items.py
+++ Invoice Client Library/lib/items.py
@@ -1,74 +1,29 @@
 # coding: utf-8
 
 # Module Dependencies
-import os
-import util
-from dotenv import load_dotenv
-import requests
-
-load_dotenv()
+from response import Response
 
 class Items:
-    def __init__(self,url=None,authcode=None,organizationId=None):
-        self.url="https://invoice.zoho.in/api/v3/"
-        self.organizationId=os.getenv('organizationId')
-        code=util.Utils()
-        self.authcode=code.refreshaccessToken()
-   
+    def __init__(self):
+        self.url="https://invoice.zoho.in/api/v3/items"
+        self.res=Response()      
     
     #To create a new item
     def createItem(self, parameters):
-        newurl=self.url+"items"
-        custom_header = {
-            "Authorization": "Zoho-oauthtoken "+self.authcode,
-            "X-in-zoho-invoice-organizationid":self.organizationId,
-            "Content-Type":"application/json"
-        }
-        value={'JSONString':str(parameters)}
-        res = requests.post(newurl, headers = custom_header, params=value)
-        return res.json()
+        return self.res.getResposnse("post", self.url, parameters)
 
     #To get the item by ID
     def getItemById(self, item_id):
-        newurl=self.url+"items/"+item_id
-        custom_header = {
-            "Authorization": "Zoho-oauthtoken "+self.authcode,
-            "X-in-zoho-invoice-organizationid":self.organizationId,
-            "Content-Type":"application/json"
-        }
-        res = requests.get(newurl,headers=custom_header)
-        return res.json()
+        return self.res.getResposnse("get", self.url+item_id)
 
     #To get all the item data
     def getitemData(self):
-        newurl=self.url+"items"
-        custom_header = {
-            "Authorization": "Zoho-oauthtoken "+self.authcode,
-            "X-in-zoho-invoice-organizationid":self.organizationId,
-            "Content-Type":"application/json"
-        }
-        res = requests.get(newurl,headers=custom_header)
-        return res.json()
+        return self.res.getResposnse("get", self.url)
 
     #To update a item data
     def updateItem(self, item_id, parameters):
-        newurl=self.url+"contacts/"+item_id
-        custom_header = {
-            "Authorization": "Zoho-oauthtoken "+self.authcode,
-            "X-in-zoho-invoice-organizationid":self.organizationId,
-            "Content-Type":"application/json"
-        }
-        value={'JSONString':str(parameters)}
-        res = requests.put(newurl, headers = custom_header, params=value)
-        return res.json()
+        return self.res.getResposnse("put", self.url+item_id, parameters)       
     
     #To delete a item
     def deleteItem(self, item_id):
-        newurl=self.url+"contacts/"+item_id
-        custom_header = {
-            "Authorization": "Zoho-oauthtoken "+self.authcode,
-            "X-in-zoho-invoice-organizationid":self.organizationId,
-            "Content-Type":"application/json"
-        }
-        res = requests.delete(newurl,headers=custom_header)
-        return res.json()
+        return self.res.getResposnse("delete", self.url+item_id)
